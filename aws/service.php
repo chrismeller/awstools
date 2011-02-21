03b47ae352906bd9c86edb7f9533ca7c2f7cff28
Chris Meller <chris@doesnthaveone.com>
2011-02-20 22:32:32 -0500

    Add the abstract AWS_Service class that all services will extend.

---
+++ aws/service.php
@@ -0,0 +1,13 @@
+<?php
+
+	abstract class AWS_Service extends AWS {
+		
+		protected $api_version;
+		protected $endpoint;
+		protected $xml_namespace;
+		
+		protected $signature_version;
+		
+	}
+
+?>

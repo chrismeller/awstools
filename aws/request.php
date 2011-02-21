b50e04ce2dc106d8c0e3180da3cf197f944bf75b
Chris Meller <chris@doesnthaveone.com>
2011-02-20 23:22:07 -0500

    Fix an encoding problem with using http_build_query on lower than 5.3.6 by emulating it manually when building the body.

--- aws/request.php
+++ aws/request.php
@@ -29,7 +29,18 @@
 		
 		private function generate_body ( ) {
 			
+			// note that http_build_query's enc_type argument would solve this, but we don't have 5.3.6 reliably
 			
+			$body = array();
+			foreach ( $this->parameters as $k => $v ) {
+				
+				$body[] = rawurlencode( $k ) . '=' . rawurlencode( $v );
+				
+			}
+			
+			$body = implode('&', $body);
+			
+			return $body;
 			
 		}
 		

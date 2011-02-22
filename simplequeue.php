e0923a168b4377af5a7401f54107503f71f69a50
Chris Meller <chris@doesnthaveone.com>
2011-02-22 00:30:41 -0500

    Add the get_queue_attributes method to SQS.

--- simplequeue.php
+++ simplequeue.php
@@ -36,6 +36,60 @@
 			
 		}
 		
+		/**
+		 * Returns one or all attributes of a queue.
+		 * 
+		 * @param $queue_name string The name of the queue to fetch attributes for.
+		 * @param $attributes string|array The attribute name (or array of names) to fetch. Default: All
+		 * 		Available attributes:
+		 * 			- All
+		 * 			- ApproximateNumberOfMessages - approximate number of visible messages in the queue
+		 * 			- ApproximateNumberOfMessagesNotVisibile - approximate number of messages that are not timed-out and not deleted
+		 * 			- VisibilityTimeout
+		 * 			- CreatedTimestamp
+		 * 			- LastModifiedTimestamp
+		 * 			- Policy
+		 * 			- MaximumMessageSize
+		 * 			- MessageRetentionPeriod
+		 * 			- QueueArn
+		 */
+		public function get_queue_attributes ( $queue_url, $attributes = array('All'), $options = array() ) {
+		
+			// sqs uses the peculiar method of setting the endpoint to the queue_url, not adding a QueueName attribute like every other service would
+			$this->endpoint = $queue_url;
+			
+			// we prepend http:// or https:// as necessary, so trim that off the beginning of the URL
+			$this->endpoint = str_replace( 'http://', '', $this->endpoint );
+			$this->endpoint = str_replace( 'https://', '', $this->endpoint );
+		
+			if ( !is_array( $attributes ) ) {
+				$attributes = array( $attributes );
+			}
+			
+			$i = 1;
+			foreach ( $attributes as $v ) {
+				$options['AttributeName.' . $i] = $v;
+				$i++;
+			}
+			
+			$result = $this->request( 'GetQueueAttributes', $options, '//aws:Attribute' );
+			
+			// the results for this call are particularly cumbersome, so make them better
+			$a = array();
+			foreach ( $result->response as $attribute ) {
+				$name = (string)$attribute->Name;
+				$value = (string)$attribute->Value;
+				
+				$a[ $name ] = $value;
+			}
+			
+			// replace the response
+			$result->response = $a;
+			
+			return $result;
+			
+		}
+		
 	}
 
 ?>
